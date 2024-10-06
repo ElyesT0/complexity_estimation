86b45f3ee4be98c3a65e6202ff158d71874dfbfd
elyesT <elyes.tabbane@learningplanetinstitute.org>
2024-10-06 10:35:25 +0200

    Initial commit: minimal-version

---
+++ 1-Scripts/stimulation_scripts/dist/WelcomePage.73196939.js
@@ -0,0 +1,2 @@
+sendData=function(e,t,n,a){var p=new XMLHttpRequest;p.open("POST","https://neurospin-data.cea.fr/experiments",!0),p.setRequestHeader("Content-Type","application/x-www-form-urlencoded"),p.send("uuid="+e+"&user="+t+"&project="+n+"&data="+a)};
+//# sourceMappingURL=WelcomePage.73196939.js.map
---
+++ 1-Scripts/stimulation_scripts/dist/animation_sequence.f54ac0f9.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=animation_sequence.f54ac0f9.js.map
